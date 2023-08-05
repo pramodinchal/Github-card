624672fadce2aae8f2b53ffaaec66e82906e7dc7
Pramod Inchal <inchalpramod@gmail.com>
2023-08-05 21:16:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+// src/App.js
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [username, setUsername] = useState('');
+  const [userData, setUserData] = useState(null);
+
+  const handleInputChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const fetchUserData = async () => {
+   await axios
+      .get(`https://api.github.com/users/${username}`)
+      .then((response) => setUserData(response.data))
+      .catch((error) => console.error('Error fetching user data:', error));
+      
+
+    };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (username.trim() !== '') {
+      fetchUserData();
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>GitHub User Card</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter GitHub Username"
+          value={username}
+          onChange={handleInputChange}
+        />
+        <button type="submit">Show Card</button>
+      </form>
+
+      {userData && (
+        <div className="github-card">
+          <img src={userData.avatar_url} alt="GitHub Avatar" />
+          <h2>{userData.login}</h2>
+          <p>Name: {userData.name}</p>
+          <p>Public Repos: {userData.public_repos}</p>
+          <p>Public Gists: {userData.public_gists}</p>
+          <p>Profile Created: {userData.created_at}</p>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
